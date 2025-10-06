7229e857043c78ef7391332aa596d7853c6c3f3e
Eduardo Caetano <eduardocaetano110@gmail.com>
2025-10-06 19:01:23 -0300

    [Feat]: Estrura a entidade AnimeModel com atributos e anotações

--- src/main/java/dev/java10x/animesearchia/Model/AnimeModel.java
+++ src/main/java/dev/java10x/animesearchia/Model/AnimeModel.java
@@ -1,9 +1,11 @@
 package dev.java10x.animesearchia.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import dev.java10x.animesearchia.Model.Enum.Generos;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "anime_catalogo")
@@ -11,5 +13,88 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AnimeModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String sinopse;
+    private Generos Animegeneros;
+    private Integer numeroEpisodios;
+    private Integer numeroTemporadas;
+    private LocalDate dataLancamento;
+    private String classificacaoIndicativa;
+    private String urlImagemCapa;
 
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public Generos getAnimegeneros() {
+        return Animegeneros;
+    }
+
+    public void setAnimegeneros(Generos animegeneros) {
+        Animegeneros = animegeneros;
+    }
+
+    public Integer getNumeroEpisodios() {
+        return numeroEpisodios;
+    }
+
+    public void setNumeroEpisodios(Integer numeroEpisodios) {
+        this.numeroEpisodios = numeroEpisodios;
+    }
+
+    public Integer getNumeroTemporadas() {
+        return numeroTemporadas;
+    }
+
+    public void setNumeroTemporadas(Integer numeroTemporadas) {
+        this.numeroTemporadas = numeroTemporadas;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public String getClassificacaoIndicativa() {
+        return classificacaoIndicativa;
+    }
+
+    public void setClassificacaoIndicativa(String classificacaoIndicativa) {
+        this.classificacaoIndicativa = classificacaoIndicativa;
+    }
+
+    public String getUrlImagemCapa() {
+        return urlImagemCapa;
+    }
+
+    public void setUrlImagemCapa(String urlImagemCapa) {
+        this.urlImagemCapa = urlImagemCapa;
+    }
 }
