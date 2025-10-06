1c146ce6bbef847903216c47baabe1e7284a2d2d
Eduardo Caetano <eduardocaetano110@gmail.com>
2025-10-06 19:47:29 -0300

    [Feat]: Cria as rotas de adicionar e listar animes

--- src/main/java/dev/java10x/animesearchia/Controller/AnimeController.java
+++ src/main/java/dev/java10x/animesearchia/Controller/AnimeController.java
@@ -1,8 +1,33 @@
 package dev.java10x.animesearchia.Controller;
 
-import org.springframework.stereotype.Controller;
+import dev.java10x.animesearchia.Model.AnimeDTO;
+import dev.java10x.animesearchia.Service.AnimeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/anime")
 public class AnimeController {
+    private final AnimeService animeService;
+
+    public AnimeController(AnimeService animeService) {
+        this.animeService = animeService;
+    }
+
+    //post
+    @PostMapping("/adiciona")
+    public ResponseEntity<AnimeDTO> adicionaAnime(@RequestBody AnimeDTO anime) {
+        AnimeDTO novoAnime = animeService.adicionaAnime(anime);
+        return ResponseEntity.ok(novoAnime);
+    }
+    //get
+    @GetMapping("/listar")
+    public ResponseEntity<List<AnimeDTO>> listarAnimes() {
+        List<AnimeDTO> animes = animeService.listarAnimes();
+        return ResponseEntity.ok(animes);
+    }
+    //update
+    //delete
 }
--- src/main/java/dev/java10x/animesearchia/Service/AnimeService.java
+++ src/main/java/dev/java10x/animesearchia/Service/AnimeService.java
@@ -1,7 +1,37 @@
 package dev.java10x.animesearchia.Service;
 
+import dev.java10x.animesearchia.Model.AnimeDTO;
+import dev.java10x.animesearchia.Model.AnimeMapper;
+import dev.java10x.animesearchia.Model.AnimeModel;
+import dev.java10x.animesearchia.Repository.AnimeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AnimeService {
+    private final AnimeRepository repository;
+    private final AnimeMapper mapper;
+
+    public AnimeService(AnimeRepository repository, AnimeMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    //Adiciona Anime ---> POST
+    public AnimeDTO adicionaAnime(AnimeDTO animeDTO) {
+        AnimeModel anime = new AnimeMapper().map(animeDTO);
+        anime = repository.save(anime);
+        return mapper.map(anime);
+    }
+
+    //Lista os animes ---> GET
+    public List<AnimeDTO> listarAnimes() {
+        List<AnimeModel> animes = repository.findAll();
+        return animes.stream()
+                .map(mapper::map)
+                .collect(Collectors.toList());
+
+    }
 }
