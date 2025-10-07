c7d27a6da9fddfd8a4006aec10aeca067d2eb5b1
Eduardo Caetano <eduardocaetano110@gmail.com>
2025-10-07 00:46:28 -0300

    [Feat]: Rotas Criadas-  adicionar novo anime, alterar, listar tanto por id ou todos e de deleção

--- src/main/java/dev/java10x/animesearchia/Controller/AnimeController.java
+++ src/main/java/dev/java10x/animesearchia/Controller/AnimeController.java
@@ -2,6 +2,7 @@ package dev.java10x.animesearchia.Controller;
 
 import dev.java10x.animesearchia.Model.AnimeDTO;
 import dev.java10x.animesearchia.Service.AnimeService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,12 +23,42 @@ public class AnimeController {
         AnimeDTO novoAnime = animeService.adicionaAnime(anime);
         return ResponseEntity.ok(novoAnime);
     }
-    //get
+    //get listar geral
     @GetMapping("/listar")
-    public ResponseEntity<List<AnimeDTO>> listarAnimes() {
+    public ResponseEntity<List<AnimeDTO>> listarTodosAnimes() {
         List<AnimeDTO> animes = animeService.listarAnimes();
         return ResponseEntity.ok(animes);
     }
+
+    //Listar por ID
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<?> listarAnimeId(@PathVariable Long id) {
+        AnimeDTO anime = animeService.listarPorId(id);
+        if(anime != null) {
+            return ResponseEntity.ok(anime);
+        }else  {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Anime não encontrado");
+        }
+    }
+
     //update
+    @PutMapping("/alterar/{id}")
+    public ResponseEntity<?>alterarAnimePorId(@PathVariable Long id, @RequestBody AnimeDTO anime) {
+        AnimeDTO alteraAnime = animeService.atualizaAnimePorId(id, anime);
+        return ResponseEntity.ok(alteraAnime);
+    }
+
     //delete
+    @DeleteMapping("/deletar")
+    public ResponseEntity<?> deletarAnimePorId(@PathVariable Long id) {
+        if(animeService.listarPorId(id) != null){
+            animeService.removerAnimePorId(id);
+            return ResponseEntity.ok("Anime removido com sucesso");
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Anime do (ID): "+id+" não encontrado");
+        }
+    }
+
 }
--- src/main/java/dev/java10x/animesearchia/Service/AnimeService.java
+++ src/main/java/dev/java10x/animesearchia/Service/AnimeService.java
@@ -7,6 +7,7 @@ import dev.java10x.animesearchia.Repository.AnimeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -26,12 +27,35 @@ public class AnimeService {
         return mapper.map(anime);
     }
 
-    //Lista os animes ---> GET
+    //Listar os animes ---> GET
     public List<AnimeDTO> listarAnimes() {
         List<AnimeModel> animes = repository.findAll();
         return animes.stream()
                 .map(mapper::map)
                 .collect(Collectors.toList());
 
+    }
+
+    //Listar por Id
+    public AnimeDTO listarPorId(Long id) {
+        Optional<AnimeModel> anime = repository.findById(id);
+        return anime.map(mapper::map).orElse(null);
+    }
+
+    //Alterar anime por id
+    public AnimeDTO atualizaAnimePorId(Long id, AnimeDTO animeDTO) {
+        Optional<AnimeModel> animeExistente = repository.findById(id);
+        if (animeExistente.isPresent()) {
+            AnimeModel animeAtualizado = mapper.map(animeDTO);
+            animeAtualizado.setId(id);
+            animeAtualizado = repository.save(animeAtualizado);
+            return mapper.map(animeAtualizado);
+        }else {
+            return null;
+        }
+    }
+    //delete
+    public void removerAnimePorId(Long id) {
+        repository.deleteById(id);
     }
 }
